630ddc32c5c2ff4164bc2e5a64983ec341e31bd9
Notwinner <iliazukov61@gmail.com>
2025-08-01 22:58:49 +0300

    visual

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 import '../styles/main.scss'; // Импорт SCSS в JS (Webpack обработает)
+
+const tasks = document.querySelectorAll('.task');
+
+tasks.forEach(task => {
+    task.addEventListener('click', () => {
+        task.classList.toggle('done');
+        var taskLabel = task.querySelector('.status-label');
+        taskLabel.textContent = task.classList.contains('done') ? 'finished' : 'unfinished';
+    });
+
+});
